0e10dda2394a74154e36169ce00a66a6324afb50
Josa Vicente <jvicenpe@gmail.com>
2022-12-30 20:49:13 +0100

    Step 4

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -6,16 +6,18 @@ import kotlin.system.exitProcess
 
 fun main() {
     println("Please enter your birthday with the format <yyyy-MM-dd>")
-    val line = supportNullableString(readLine())
-    if (line.isNullOrEmpty()) {
-        println("The date is not valid")
-        exitProcess(1)
+    supportNullableString(readLine()).takeUnless {
+        it.isNullOrEmpty() || it.isNullOrBlank()
+    }?.let{ line ->
+        val inputDate = LocalDate.parse(line).also{
+            println("You wrote $it")
+        }
+        with(Period.between(inputDate, LocalDate.now())) {
+            println("The difference between the date you wrote and today is ${this.years}")
+        }
+
     }
-    val inputDate = LocalDate.parse(line)
-    println("You wrote $inputDate")
-    val currentDate = LocalDate.now()
-    val difference = Period.between(inputDate, currentDate)
-    println("The difference between the date you wrote and today is ${difference.years}")
+
 }
 
 fun supportNullableString(line: String?) = line
